dd2481dd4d93a7b99fae50f326c5bd16aed7d8bf
Victor Chen <victor@Victors-MacBook-Pro.local>
2019-05-20 21:04:19 -0700

    Update index.html with correct references and complete getSenators method

--- js/logic.js
+++ js/logic.js
@@ -21,21 +21,40 @@ var svg = d3.select("#scatter")
 
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
-  
-// Import Data Using File instead of URL for now
+
+
+//getSenators will take in the dataset and a year as a string, currently a console lo
+function getSenators(d, yearString) {
+  var i = 0;
+  var results = []
+  d.forEach(e => {
+    e.terms.forEach(f => {
+      var formatYear = d3.timeFormat("%Y");
+      if (f.type == "sen" && parseInt(yearString, 10) >= parseInt(formatYear(new Date(f.start)), 10) &&
+        parseInt(yearString, 10) < parseInt(formatYear(new Date(f.end)), 10)) {
+        var senator = {};
+        senator.name = e.name;
+        senator.start = f.start;
+        senator.end = f.end;
+        senator.state = f.state;
+        senator.party = f.party;
+        results.push(senator);
+        console.log("-------------------------")
+        console.log(e.name);
+        console.log(f);
+        console.log(`start: ${parseInt(formatYear(new Date(f.start)), 10)}`);
+        console.log(parseInt(yearString, 10))
+        console.log(`end: ${parseInt(formatYear(new Date(f.end)), 10)}`)
+        console.log(i);
+        i = i + 1;
+      }
+    });
+  });
+  return results;
+}
+
+//Import Data Using File Instead of Flask, for now
 d3.json("./data/legislators-historical.json",d => {
       console.log(d);
-      d.forEach(e => {
-          e.terms.forEach(f => {
-            if(f.type == "sen"){
-                //console.log(f);
-            }
-          });
-      });
+      console.log(getSenators(d,'1972'));
 });
-
-//Testing Dates
-var blah = Date("2019-01-02")
-console.log(blah)
-console.log(typeof blah)
-console.log(parseDate(blah))
