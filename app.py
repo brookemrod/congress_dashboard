a9fcb49fd25c88772453739f1420ad316ef46373
jwiner97 <jason.winer@gmail.com>
2019-05-20 20:56:27 -0700

    app.py update

--- app.py
+++ app.py
@@ -17,25 +17,11 @@ collection = db['congressman']
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
-    db.collection.drop()
     with open('data/legislators-historical.json') as data_file:
         data = json.load(data_file)
-        for item in data:
-            # key = {"id": item['id']['govtrack']}
-            # data = {first_name: item['name']['first'],
-            #     last_name: item['name']['last'],
-            #     dob: item['bio']['birthday'],
-            #     gender: item['bio']['gender'],
-            #     state: item['terms']['state'],
-            #     startTerm: item['terms']['start'],
-            #     endTerm: item['terms']['end'],
-            #     party: item['terms']['party'],
-            #     legislatorType: item['terms']['type']
-            #     }
-            mongo.db.collection.insert_one.(item)
     # Find one record of data from the mongo database
-    congress_data = mongo.db.collection.find_all.()
-    
+    for d in data:
+        collection.insert_one(d)  
     # Return template and data
     return render_template("index.html", congress_data=congress_data)
 
